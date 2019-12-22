cf99d33a5ed7df75eaeef50c7cc26538c1680360
lisp719 <lisp719@gmail.com>
2019-12-22 20:02:21 +0900

    md-editorのrouteを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MdEditorComponent } from "./md-editor/md-editor.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "md-editor",
+    component: MdEditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdEditorComponent } from './md-editor/md-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MdEditorComponent
   ],
   imports: [
     BrowserModule,
