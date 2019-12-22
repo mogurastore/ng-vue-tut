f3cd45b276f655b8ee6f025b132f8d6bc28ac5a4
lisp719 <lisp719@gmail.com>
2019-12-22 20:19:32 +0900

    markdownエディターの実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MdEditorComponent } from './md-editor/md-editor.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MdEditorComponent } from "./md-editor/md-editor.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MdEditorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MdEditorComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/md-editor/md-editor.component.ts
+++ src/app/md-editor/md-editor.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { of } from "rxjs";
+import { startWith, debounceTime, map } from "rxjs/operators";
+import * as marked from "marked";
 
 @Component({
-  selector: 'app-md-editor',
-  templateUrl: './md-editor.component.html',
-  styleUrls: ['./md-editor.component.css']
+  selector: "app-md-editor",
+  templateUrl: "./md-editor.component.html",
+  styleUrls: ["./md-editor.component.css"]
 })
 export class MdEditorComponent implements OnInit {
+  input = new FormControl("# hello");
+  compiledMarkdown$ = of("");
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.compiledMarkdown$ = this.input.valueChanges.pipe(
+      startWith(this.input.value),
+      debounceTime(300),
+      map(s => marked(s, { sanitize: true }))
+    );
   }
 
+  ngOnInit() {}
 }
