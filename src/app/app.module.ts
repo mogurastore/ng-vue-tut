0b220bab2d34d9c91800d68095ca27b56f019c1f
lisp719 <lisp719@gmail.com>
2019-12-26 01:50:24 +0900

    githubのcommitを取得できるようにした

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MdEditorComponent } from "./md-editor/md-editor.component";
-import { GithubCommitsComponent } from './github-commits/github-commits.component';
+import { GithubCommitsComponent } from "./github-commits/github-commits.component";
 
 @NgModule({
   declarations: [AppComponent, MdEditorComponent, GithubCommitsComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/github-commits/github-commits.component.ts
+++ src/app/github-commits/github-commits.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { of } from "rxjs";
+import { startWith, switchMap } from "rxjs/operators";
+
+const apiURL = "https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=";
 
 @Component({
-  selector: 'app-github-commits',
-  templateUrl: './github-commits.component.html',
-  styleUrls: ['./github-commits.component.css']
+  selector: "app-github-commits",
+  templateUrl: "./github-commits.component.html",
+  styleUrls: ["./github-commits.component.css"]
 })
 export class GithubCommitsComponent implements OnInit {
+  branches = ["master", "dev"];
+  currentBranch = new FormControl("master");
+  commits = of([]);
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.commits = this.currentBranch.valueChanges.pipe(
+      startWith(this.currentBranch.value),
+      switchMap(branch => this.http.get<Object[]>(apiURL + branch))
+    );
   }
 
+  ngOnInit() {}
 }
